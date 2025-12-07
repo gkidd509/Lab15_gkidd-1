1bb67b2148932e85ae4075a39414fde8ae6037b2
Gkidd509 <gkidd509@gmail.com>
2025-12-06 22:23:58 -0500

    Plotted a chevron shape using specifically defined points and added an option to choose which shape to view

--- plot.py
+++ plot.py
@@ -7,13 +7,42 @@ This program uses matplotlib to plot various graphs
 import matplotlib.pyplot as plt
 import numpy as np
 
-S = 2
-t = np.linspace(0, 2*np.pi, 1000)
 
-x = np.power(np.cos(t), S) * np.cos(t)
-y = np.power(np.cos(t), S) * np.sin(t)
 
-fig, ax = plt.subplots()
-ax.plot(x, y)
-plt.show()
+def infinity_plot():
+    s = 2
+    t = np.linspace(0, 2*np.pi, 1000)
+
+    x = np.power(np.cos(t), s) * np.cos(t)
+    y = np.power(np.cos(t), s) * np.sin(t)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y, lw=3)
+    plt.show()
+
+def chevron():
+    x = [1, 3, 8, 4, 3, 1]
+    y = [1, 9, 2, 4, 3, 1]
+
+    plt.plot(x, y)
+    plt.show()
+
+class Main:
+    plot_map = {
+        1: infinity_plot,
+        2: chevron
+    }
+
+    print("Welcome to my cool graphs! Pick the number of the graph you would like to see!")
+    print(f"1: Infinity Symbol")
+    print(f"2: Chevron Symbol")
+    user_input = int(input("Enter the number of the shape you want: "))
+
+    if user_input in plot_map:
+        plot_map[user_input]()
+    else:
+        print("invalid input")
+
+if __name__ == "__main__":
+    Main()
 
